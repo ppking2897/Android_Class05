5bcb5c89949fca24d7f47688a2b0a311d38c82e4
ppking <ppking2897@gmail.com>
2016-11-17 10:01:59 +0800

    Class05

---
+++ app/src/main/java/com/example/user/ppking_android_class05/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.ppking_android_class05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/user/ppking_android_class05/My_View.java
@@ -0,0 +1,17 @@
+package com.example.user.ppking_android_class05;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 2016/11/17.
+ */
+
+public class My_View extends View {
+    public My_View(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setBackgroundColor(Color.BLUE);
+    }
+}
